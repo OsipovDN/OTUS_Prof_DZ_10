8b6b15dfce439530a3135cdcdece7d339a1c592e
OsipovDN <98536098+OsipovDN@users.noreply.github.com>
2024-07-12 20:00:49 +0300

    Исправления для мержа

--- async/src/libasync.cpp
+++ async/src/libasync.cpp
@@ -10,37 +10,16 @@
 
 namespace async {
 
-	class HandlerKeeper {
-	public:
-		HandlerKeeper():_printerPtr(nullptr),_queuePtr(nullptr){};
-		void setPrinter(std::shared_ptr<IPrinter> printerPtr) { _printerPtr = printerPtr; }
-		void setQueue(std::shared_ptr<IQueue> queuePtr) { _queuePtr = queuePtr; }
-		std::shared_ptr<IPrinter> getPrinter() {return _printerPtr; }
-		std::shared_ptr<IQueue> getQueue() { return _queuePtr; }
-		void remove() 
-		{
-			_printerPtr = nullptr;
-			_queuePtr = nullptr;
-		}
-
-	private:
-		std::shared_ptr<IPrinter> _printerPtr;
-		std::shared_ptr<IQueue> _queuePtr;
-	};
-
-	HandlerKeeper helper;
-
 	handle_t connect(std::size_t bulk) {
 
-		helper.setQueue (std::make_shared<msg::MassageQueue>());
-		helper.setPrinter(std::make_shared<Printer>(helper.getQueue(), 2));
-
-		return std::make_unique<Controller::CommandController>(helper.getQueue(), bulk).release();
+		static auto msgSender = std::make_shared<msg::MassageQueue>();
+		static auto filePrinter = std::make_shared<Printer>(msgSender, 2);
+		return std::make_unique<Controller::CommandController>(msgSender, bulk).release();
 	}
 
 	void receive(handle_t handler, const char* data, std::size_t size) {
 
-		auto controller = reinterpret_cast<IController*>(handler);
+		auto controller = reinterpret_cast<Controller::CommandController*>(handler);
 		if (handler == nullptr)
 			return;
 
@@ -56,7 +35,7 @@ namespace async {
 
 	void disconnect(handle_t handler) {
 		receive(std::move(handler), "EOF", 3);
-		auto controller = reinterpret_cast<IController*>(handler);
+		auto controller = reinterpret_cast<Controller::CommandController*>(handler);
 		controller->finish();
 	}
 
