21bce470c927d81e7dec13d533dc65f4cae11447
OsipovDN <98536098+OsipovDN@users.noreply.github.com>
2024-07-12 20:39:59 +0300

    IQueue

--- async/headers/IQueue.h
+++ async/headers/IQueue.h
@@ -1,12 +1,12 @@
 #pragma once
 
-class IQueue 
+class IQueue
 {
 public:
 
 	virtual void push(std::vector <std::string>& massage) = 0;
 	virtual void pop() = 0;
-	virtual std::vector <std::string>& front() = 0;
+	virtual std::vector <std::string> front() = 0;
 	virtual bool empty() const = 0;
 	virtual bool isFinish()const = 0;
 	virtual void finish() = 0;
